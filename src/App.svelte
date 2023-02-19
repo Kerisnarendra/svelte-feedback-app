7403c13a1c0e9df8cb2ecc25787a4cf942dd83c6
Kerisnarendra <kakerisn@gmail.com>
2023-02-19 18:25:01 +0800

    Implement feedback app

--- src/App.svelte
+++ src/App.svelte
@@ -1,64 +1,41 @@
 <script>
-	let firstName = 'Brad';
-	let lastName = 'Traversy';
-	let color = 'blue';
-	let showText = false;
+	import FeedbackList from "./components/feedbackList.svelte";
+	import FeedbackStas from "./components/FeedbackStas.svelte";
+	import FeedbackForm from "./components/FeedbackForm.svelte";
 
-	let users = [
+	let feedback = [
 		{
 			id: 1,
-			name: 'Putu'
+			rating: 10,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
 		},
 		{
 			id: 2,
-			name: 'Kadek'
+			rating: 9,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
 		},
 		{
 			id: 3,
-			name: 'Komang'
-		}				
-	]
+			rating: 8,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		}
+	];
+	$: count = feedback.length;
+	$: average = feedback.reduce((a, {rating}) => a + rating, 0)/ feedback.length;
 
-	$: name = firstName + " " + lastName;	
-	const toggle = () => {
-		color = color === 'blue' ? 'red' : 'blue';
-		showText = !showText;
-		users = [...users, {id: 4, name: 'Ketut'}]
+	const deleteFeedback = (e) => {
+		const itemId = e.detail;
+
+		feedback = feedback.filter((item) => item.id != itemId)
+	}
+
+	const addFeedback = (e) => {
+		feedback = [e.detail, ...feedback]
 	}
 </script>
 
-<main>
-	<h1 style="color: {color}">Hello {name}!</h1>
-	{#if showText}
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	{:else}
-	<p>No text</p>
-	{/if}
-	<button on:click={toggle}>Click</button>
-
-	{#each users as user}
-		<h3>{user.id}: {user.name}</h3>
-	{/each}
+<main class="container">
+	<FeedbackForm on:add-feedback={addFeedback}/>
+	<FeedbackStas count={count} average={average}/>
+	<FeedbackList feedback={feedback} on:delete-feedback={deleteFeedback}/>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
